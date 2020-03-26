945c631d7bc85e2b12fae2350cd992ec7bd25ee5
zainabzain2728 <62622987+zainabzain2728@users.noreply.github.com>
2020-03-26 16:19:09 +0500

    working on First Express App

--- webdevbootcamp/FirstExpressApp/app.js
+++ webdevbootcamp/FirstExpressApp/app.js
@@ -1,2 +1,23 @@
 var express = require("express");
 var app = express();
+
+// "/" =>"hi there"
+
+app.get("/", function(req, res) {
+  res.send("Hi there!");
+});
+
+// "/bye" => "good bye"
+app.get("/bye", function(req, res) {
+  res.send("goodbye");
+});
+// "/dog" => "mewo"
+
+app.get("/dog", function(req, res) {
+  res.send("meow");
+});
+
+//tell express to listenfor request(start server)
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("server started");
+});
