884cd3f64cd6c370289811b1bcc3e81398c6e446
zainabzain2728 <62622987+zainabzain2728@users.noreply.github.com>
2020-03-30 23:10:51 +0500

    conditions and loops

--- webdevbootcamp/MoreExpress/EJSDemo/appy.js
+++ webdevbootcamp/MoreExpress/EJSDemo/appy.js
@@ -1,13 +1,25 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
   var thing = req.params.thing;
-  res.render("love.ejs", { thingVar: thing });
+  res.render("love", { thingVar: thing });
+});
+
+app.get("/posts", function(req, res) {
+  var posts = [
+    { title: "post 1", auther: "susy" },
+    { title: "post 2", auther: "zain" },
+    { title: "post 3", auther: "zaini" }
+  ];
+  res.render("posts", { posts: posts });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(req, res) {
