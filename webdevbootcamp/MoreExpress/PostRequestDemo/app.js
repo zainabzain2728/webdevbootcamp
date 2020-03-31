72299a841a8dd0a0e1e7f8dddec4b12c315010dc
zainabzain2728 <62622987+zainabzain2728@users.noreply.github.com>
2020-03-31 15:44:59 +0500

    ejs post request concept

---
+++ webdevbootcamp/MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+var friends = ["zain", " zaini", "ali", "tasmia"];
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.post("/addfriend", function(req, res) {
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
+});
+
+app.get("/friends", function(req, res) {
+  res.render("friends", { friends: friends });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("sever start");
+});
